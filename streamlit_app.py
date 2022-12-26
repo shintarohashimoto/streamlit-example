dda0ea0d59ac3cc052155344024762da6ae4344d
ゆとり <87046090+shintarohashimoto@users.noreply.github.com>
2022-12-26 16:27:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,18 @@
 import streamlit as st
-
+import qrcode
+import tempfile
+import os
 from PIL import Image
-import glob, os
-import sys
 
-st.file_uploader(label="画像をアップロードしてください",type=["jpeg","png"],accept_multiple_files=True)
+
+sei = st.text_input('姓')
+mei = st.text_input('名')
+seinengappi = st.text_input('生年月日')
+value = sei+mei+seinengappi
+if value:
+    with tempfile.TemporaryDirectory() as folder:
+        img = qrcode.make(value)
+        path = os.path.join(folder, "test.png")
+        img.save(path)
+        with Image.open(path) as f:
+            st.image(f, caption='Sunrise by the mountains')
